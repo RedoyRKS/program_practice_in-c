620c014e1e2e9512fd471640e79b48abf4721f79
Redoy Kumar Saha <83264166+RedoyRKS@users.noreply.github.com>
2022-02-23 23:36:56 +0600

    Add files via upload

---
+++ Ascci.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    /* 
+    int n;
+    printf("Enter a ASCII Value : ");
+    scanf("%d",&n);
+    printf("The ASSCI charecter is : %c\n",n);
+    */
+
+
+    char x;
+    printf("Enter a ASCII charecte : ");
+    scanf("%c",&x);
+    printf("The ASSCI value is : %d\n",x);
+
+
+
+    //print the ASCCI value to my name.
+    int A=82,B=101,C=100,D=111,E=121;
+    printf("My name is %c%c%c%c%c Saha",A,B,C,D,E);
+
+    return 0;
+}
